1418efd3a284ea85f3a80035a10cba5bbfc4b92e
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-10-12 15:54:24 +0900

    add alert when no input text_field and image

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,20 +1,22 @@
 $(function(){
   function buildHTML(message) {
-    function image() {
-      message.image != null ? `<img src=${message.image} class="lower-message__image"></img>`:''
-    };
+    var image = (message.image) ? `<img src=${message.image} class="lower-message__image">`:'';
     var html = `<div class="message">
                   <p class="body--title">${message.user_name}</a>
                   <div class="body--message">
                     ${message.date}
                   </div>
                   <p class="body--text">${message.content}</p>
-                  ${image()}
+                  ${image}
                 </div>`
     return html;
   }
   $('#new_message').on('submit',function(e){
     e.preventDefault();
+    if($('#message_content').val() === '' && $('#message_image').val() === ''){
+      alert('入力してください');
+      return false;
+    };
     var formdata = new FormData(this);
     $.ajax({
       url: window.location.href,
@@ -25,7 +27,6 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      console.log(data);
       var html = buildHTML(data);
       $('.message-list').append(html);
       $('.input-box__btn').attr('disabled',false);
